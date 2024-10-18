84faed4f53a0467891cb78b474c98895e24d75b8
aryaW <aryawankhade25@gmail.com>
2024-10-18 18:50:58 +0530

    Fix: Line endings and code changes

--- CarZone/settings.py
+++ CarZone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'CarZone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':os.path.join(BASE_DIR ,'db.sqlite3'),
     }
 }
 
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR ,'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'CarZone/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
