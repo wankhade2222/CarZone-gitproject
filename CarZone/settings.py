fef5ffa9f0060c423d4e56ef480cb3a26c562506
aryaW <aryawankhade25@gmail.com>
2024-10-21 18:28:51 +0530

    Initial commit

--- CarZone/settings.py
+++ CarZone/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'CarZone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':os.path.join(BASE_DIR ,'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'carzone25',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+        
     }
 }
 
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    
+    # Use ImageField for photo uploads
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    
+    # Use URLField for social media links
+    instagram_link = models.CharField(max_length=255)
+    twitter_link = models.CharField(max_length=255)
+    facebook_link = models.CharField(max_length=255)
+    
+    # Use DateTimeField for timestamps
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
